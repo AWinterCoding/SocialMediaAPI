6ea0a9fb1b4e547c025068c299eb9a2c45eec0d9
Alexandra <awinterdrova@gmail.com>
2023-07-19 12:19:57 -0600

    added model data

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,21 @@
 const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+    thought: {type:String, required: true, minLength:0, maxLength:280},
+    username: {type:String, required: true},
+    createdAt: {type:Date, default: Date.now},
+    reactions:[reactionSchema]
+});
+
+const reactionSchema = new mongoose.Schema({
+    reactionID: mongoose.ObjectID,
+    reactionBody: {type:String, required:true, maxLength:280},
+    username: {type:String, required:true},
+    createdAt: {type:Date, default: Date.now}
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+const handleError = (err) => console.error(err);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,20 @@
 const mongoose = require('mongoose');
+const {thoughtSchema} = require('./Thought');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, trimmed: true},
+    email: { type: String, unique: true, required: [true, `User email is required`],
+        validate: {
+            validator: function(v) {
+                return /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/.test(v);
+            },
+            message: props => `${props.value} is not a avalid email`
+        },
+    },
+    thoughts: [thoughtSchema],
+    friends: [UserSchema],
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
