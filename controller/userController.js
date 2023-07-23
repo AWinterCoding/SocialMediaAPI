01ce84d76474b9ff84e92ca0a09c4ab093d34963
Alexandra <awinterdrova@gmail.com>
2023-07-22 20:05:28 -0600

    added population code to Thoughts

--- controller/userController.js
+++ controller/userController.js
@@ -26,8 +26,7 @@ module.exports = {
       //function to get a single user
       async getSingleUser(req, res) {
         try {
-          const user = await User.findOne({ _id: req.params.userId })
-            .select('-__v');
+          const user = await User.findOne({ _id: req.params.userId });
     
           if (!user) {
             return res.status(404).json({ message: 'No user with that ID' });
--- models/Thought.js
+++ models/Thought.js
@@ -15,9 +15,28 @@ const thoughtSchema = new mongoose.Schema({
     createdAt: {type:Date, default: Date.now},
     reactions:[reactionSchema]
 });
-
-
 const Thought = mongoose.model('Thought', thoughtSchema);
+
+thoughtlist = [
+    {thought:"thought1", username:"test4", reactions:[]},
+    {thought:"thought2", username:"test5", reactions:[]},
+    {thought:"thought3", username:"test6", reactions:[]},
+    {thought:"thought4", username:"test7", reactions:[]},
+    {thought:"thought5", username:"test8", reactions:[]},
+    {thought:"thought6", username:"test8", reactions:[]},
+];
+
+Thought.find({}).exec().then(async collection =>{
+    if(collection.length === 0){
+        const results = await Thought.insertMany(
+            thoughtlist
+        );
+        return console.log('Thoughts Successfully inserted', results);
+    }
+    return console.log('We have enough test data');
+}).catch(err => console.log(err));
+
+
 
 const handleError = (err) => console.error(err);
 
